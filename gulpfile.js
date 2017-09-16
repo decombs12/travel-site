fa20e5f1603a5962e77f462d005a090d09ca7c69
Dave <combs_dave@comcast.net>
2017-09-16 00:01:53 -0700

    began new gulpfile and app tonight

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,25 @@
-var gulp 	 = require('gulp'),
-watch 		 = require('gulp-watch'),
-postcss 	 = require('gulp-postcss'),
-autoprefixer = require('autoprefixer'),
-cssvars      = require('postcss-simple-vars'),
-nested       = require('postcss-nested'),
-cssImport    = require('postcss-import');
-
+var gulp 	= require('gulp'),
+watch 		= require('gulp-watch');
 
 gulp.task('default', function(){
 	console.log("Hooray - you created a Gulp task.");
 });
 
-gulp.task('html', function(){
-	console.log("Updated your HTML.");
+gulp.task('html', function() {
+	console.log("Something useful to your HTML has been performed - automatically.");
 });
 
-gulp.task('styles', function(){
-	return gulp.src('./app/assets/styles/styles.css')
-		.pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
-		.pipe(gulp.dest('./app/temp/styles'));
+gulp.task('styles', function() {
+	console.log("Something useful to your Styles.CSS has been performed - automatically.");
 });
 
 gulp.task('watch', function() {
-
 	watch('./app/index.html', function() {
 		gulp.start('html');
 	});
 
 	watch('./app/assets/styles/**/*.css', function() {
 		gulp.start('styles');
-	});
-
+	})
 });
+
